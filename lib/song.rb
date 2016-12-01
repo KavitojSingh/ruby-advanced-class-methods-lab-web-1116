a58a64ee167cf0c0cd9ba06347e6fb4e6e5a78a9
github username <github email address>
2016-12-01 17:26:26 -0500

    some of it is done

--- lib/song.rb
+++ lib/song.rb
@@ -6,8 +6,33 @@ class Song
     @@all
   end
 
+  def self.create
+  	self.new.save
+  end
+
+  def self.new_by_name(name)
+  	song = self.new
+  	song.name = name
+  	song 
+  end
+
+  def self.create_by_name(name)
+  	#song = self.new
+  	#song.name = name
+  	#song.save
+  	song = self.new_by_name(name)
+  	song.save
+  end
+
+  def self.find_by_name(name)
+  	self.all.find{|song| song.name == name}    #"detect" is the same exact thing as "find"
+  end
+
+  
+
   def save
     self.class.all << self
+    self
   end
 
 end
